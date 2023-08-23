079e2c449754e243ac50fd8f9117246000126ba5
Bruno Vidal <brunovidal27.19@gmail.com>
2023-08-23 20:26:07 -0300

    Correçao do codigo e geração do requerements

--- FreteApp/apps.py
+++ FreteApp/apps.py
@@ -2,5 +2,5 @@ from django.apps import AppConfig
 
 
 class FreteappConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'FreteApp'
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "FreteApp"
